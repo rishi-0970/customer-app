33d36ae6fbaabe73624c6fee20da7d4d19819142
rishi-0970 <144110499+rishi-0970@users.noreply.github.com>
2026-03-09 18:23:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+st.title("Customer Personality Clustering App")
+
+st.write("Enter customer details")
+
+income = st.number_input("Annual Income")
+spending = st.number_input("Spending Score")
+
+model = pickle.load(open("model.pkl","rb"))
+
+if st.button("Predict Cluster"):
+
+    data = np.array([[income, spending]])
+
+    cluster = model.predict(data)
+
+    st.success(f"Customer belongs to Cluster {cluster[0]}")
